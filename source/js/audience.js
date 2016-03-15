f87ed3134eae6ea4daaafb36dac1b9a0360b84f6
Sahil Dhanju <sahildhanju1@gmail.com>
2016-03-15 18:39:08 -0500

    Added audience button

--- source/js/audience.js
+++ source/js/audience.js
@@ -15,3 +15,17 @@ window.addEventListener('devicemotion', function(event)
 	acc = event;
 });
 
+var button = document.getElementById("button");
+var mytimeout = null;
+
+button.addEventListener("touchstart", buttonPress, false);
+
+function buttonPress(event) {
+  button.classList.add("active");
+  clearTimeout(mytimeout);
+  mytimeout = setTimeout(removeActive, 1000);
+}
+
+function removeActive() {
+  button.classList.remove("active")
+}
