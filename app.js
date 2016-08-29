ca71a3e45ef6616634264bc6c834ef2bbd50bca3
Devan Huapaya <huapayadevan@gmail.com>
2016-08-28 20:43:52 -0500

    old--animation-production

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ io.on('connection', function(socket)
 	});
 });
 
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 8080));
 
 http.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
